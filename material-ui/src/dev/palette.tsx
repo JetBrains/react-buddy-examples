514cf30d228f20e2cb86efc6a45b9a465bd547d9
Vlad Minaev <minaev@haulmont.com>
2021-09-14 21:33:53 +0400

    MUI Palette Initial

--- material-ui/src/dev/palette.tsx
+++ material-ui/src/dev/palette.tsx
@@ -5,13 +5,61 @@ import {
   Variant,
   Palette,
 } from '@react-buddy/ide-toolbox';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from "@mui/material/TextField";
 
 export default () => (
   <Palette>
-    <Category name="Text">
-      <Component name="Hello meessage">
+    <Category name="Layout">
+      <Component name="Box">
         <Variant>
-          <span> Hello, Create React Buddy App! </span>
+          <Box></Box>
+        </Variant>
+      </Component>
+      <Component name="Container">
+        <Variant>
+          <Container maxWidth="sm"></Container>
+        </Variant>
+        <Variant name="fixed">
+          <Container fixed></Container>
+        </Variant>
+      </Component>
+      <Component name="Grid">
+
+      </Component>
+      <Component name="Stack">
+        <Variant>
+          <Stack spacing={2}>
+            <Box>Item1</Box>
+            <Box>Item2</Box>
+          </Stack>
+        </Variant>
+        <Variant name="horizontal">
+          <Stack
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}
+          >
+            <Box>Item 1</Box>
+            <Box>Item 2</Box>
+            <Box>Item 3</Box>
+          </Stack>
+        </Variant>
+      </Component>
+    </Category>
+    <Category name="Inputs">
+      <Component name="Autocomplete">
+        <Variant>
+          <Autocomplete
+              disablePortal
+              options={[]}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Movie" />}
+          />
         </Variant>
       </Component>
     </Category>
