4db431a55a0b32d40964dd179e4a803eede2e09c
Vlad Minaev <minaev@haulmont.com>
2022-01-31 13:37:25 +0400

    Add Jhipster 7 example

---
+++ created-apps/jhipster-7/src/main/java/com/example/jh/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.example.jh.config;
---
+++ created-apps/jhipster-7/src/test/java/com/example/jh/IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.example.jh;
+
+import com.example.jh.Jh7App;
+import com.example.jh.ReactiveSqlTestContainerExtension;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = Jh7App.class)
+@ExtendWith(ReactiveSqlTestContainerExtension.class)
+public @interface IntegrationTest {
+    // 5s is the spring default https://github.com/spring-projects/spring-framework/blob/29185a3d28fa5e9c1b4821ffe519ef6f56b51962/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
