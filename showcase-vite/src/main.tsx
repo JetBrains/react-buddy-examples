84f2cbfae26944ebf360dd1465f698626c7f95a0
Vlad Minaev <minaev@haulmont.com>
2022-04-19 14:21:12 +0400

    ide-toolbox added to the vite-based showcase

--- showcase-vite/src/main.tsx
+++ showcase-vite/src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <DevSupport
+            ComponentPreviews={ComponentPreviews}
+            useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
