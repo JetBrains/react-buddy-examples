2ddf72aac093a84cf79dc0af11bfac7ae2c6a0d1
Vlad Minaev <minaev@haulmont.com>
2022-05-04 11:02:29 +0400

    Add CEF debug invocations

--- created-apps/vite-2-ts/src/App.tsx
+++ created-apps/vite-2-ts/src/App.tsx
@@ -1,9 +1,29 @@
-import { useState } from 'react'
+import {MouseEvent, useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [cefError, setCefError] = useState<string|null>(null)
+
+  function onButtonClick(event: MouseEvent<HTMLButtonElement>) {
+    // @ts-ignore
+    window.cefQuery(
+      {
+        request: (count+1).toString(),
+        onFailure: function(errorCode: any, errorMessage: any) {
+          setCefError(` error code: ${errorCode} error message: ${errorMessage}`)
+        }
+      }
+    )
+    setCount(count => {
+      return count+1;
+    });
+
+  }
+
+  // @ts-ignore
+  const typeofCef = typeof window.cefQuery;
 
   return (
     <div className="App">
@@ -11,13 +31,20 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={onButtonClick}>
+            Trigger query
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          {count}
         </p>
+        <p color={'red'}>
+          {cefError}
+        </p>
+        <p>
+          cefQuery: {typeofCef}
+        </p>
+
         <p>
           <a
             className="App-link"
